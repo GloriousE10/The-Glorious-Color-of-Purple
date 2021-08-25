7923a0ab730ed39deb9f5607f2d6499258674530
GloriousE10 <89491120+GloriousE10@users.noreply.github.com>
2021-08-24 20:11:35 -0400

    Add files via upload

---
+++ mySketch.js
@@ -0,0 +1,92 @@
+
+let numBalls = 12;
+let spring = 0.10;
+let gravity = 0.05;
+let friction = -0.009;
+let balls = [];
+
+function setup() {
+  createCanvas(windowWidth-40,500);
+	background(0);
+  for (let i = 0; i < numBalls; i++) {
+    balls[i] = new Ball(
+      random(10),
+      random(50),
+      random(30, 70),
+      i,
+      balls
+    );
+  }
+  //noStroke();
+  //fill(255, 204,255);
+}
+
+function draw() {
+  balls.forEach(ball => {
+    ball.collide();
+    ball.move();
+    ball.display();
+  });
+}
+
+class Ball {
+  constructor(xin, yin, din, idin, oin) {
+    this.x = xin;
+    this.y = yin;
+    this.vx = 0;
+    this.vy = 0;
+    this.diameter = din;
+    this.id = idin;
+    this.others = oin;
+  }
+
+  collide() {
+    for (let i = this.id + 1; i < numBalls; i++) {
+      // console.log(others[i]);
+      let dx = this.others[i].x - this.x;
+      let dy = this.others[i].y - this.y;
+      let distance = sqrt(dx * dx + dy * dy);
+      let minDist = this.others[i].diameter / 2 + this.diameter / 2;
+      //   console.log(distance);
+      //console.log(minDist);
+      if (distance < minDist) {
+        //console.log("2");
+        let angle = atan2(dy, dx);
+        let targetX = this.x + cos(angle) * minDist;
+        let targetY = this.y + sin(angle) * minDist;
+        let ax = (targetX - this.others[i].x) * spring;
+        let ay = (targetY - this.others[i].y) * spring;
+        this.vx -= ax;
+        this.vy -= ay;
+        this.others[i].vx += ax;
+        this.others[i].vy += ay;
+      }
+    }
+  }
+
+  move() {
+    this.vy += gravity;
+    this.x += this.vx;
+    this.y += this.vy;
+    if (this.x + this.diameter / 2 > width) {
+      this.x = width - this.diameter / 2;
+      this.vx *= friction;
+    } else if (this.x - this.diameter / 2 < 0) {
+      this.x = this.diameter / 2;
+      this.vx *= friction;
+    }
+    if (this.y + this.diameter / 2 > height) {
+      this.y = height - this.diameter / 2;
+      this.vy *= friction;
+    } else if (this.y - this.diameter / 2 < 0) {
+      this.y = this.diameter / 2;
+      this.vy *= friction;
+    }
+  }
+
+  display() {
+		stroke('#9B72AA');
+		fill ('#BEAEE2');
+    ellipse(this.x, this.y, this.diameter, this.diameter);
+  }
+}
